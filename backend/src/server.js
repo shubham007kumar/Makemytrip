4cd9033d71f73cee2c728801463ef68d4e3d38bd
shubham007kumar <you@example.com>
2021-07-21 11:26:53 +0530

    operator schema and model

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,13 @@ const express = require('express')
 const connect = require('./Config/db')
 const PORT = process.env.PORT || 8080
 
-
 const app = express()
 app.use(express.json())
+ 
+//-----------------post operation on travel operator---------------
+const operatorController = require('./Controller/operator.controller')
+app.use('/operator',operatorController)
+//------------------------------------------------------------------
 
 app.listen(PORT, async () =>{
     await connect()
