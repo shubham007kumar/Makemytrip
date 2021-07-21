61f8ba9d821f9a062939c61efe67175af92c3901
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-07-21 14:14:14 +0530

    Booking details schema and crud almost done

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 8080
 const app = express()
 app.use(express.json())
 
-app.listen(PORT, async () =>{
+//Booking Controller
+const bookingController = require("./controllers/booking.controller")
+app.use("/bookings", bookingController)
+
+app.listen(PORT, async () => {
     await connect()
     console.log(`Server started at ${PORT} Port`)
 })
