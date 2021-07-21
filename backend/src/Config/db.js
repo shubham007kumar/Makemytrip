a16542724aaf5ff115898604c9dcea0371dace08
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-07-21 14:18:47 +0530

    useCreateIndex added in /Config/db.json

--- backend/src/Config/db.js
+++ backend/src/Config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    return mongoose.connect('mongodb+srv://shubham:test@cluster0.zhwr1.mongodb.net/Makemytrip?retryWrites=true&w=majority',{
-        useNewUrlParser: true, 
+    return mongoose.connect('mongodb+srv://shubham:test@cluster0.zhwr1.mongodb.net/Makemytrip?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
         useUnifiedTopology: true,
+        useCreateIndex: true,
         useFindAndModify: false
     });
 }
