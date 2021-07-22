2846ac54bfb59bea5631bb6e038a01461c9bf399
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-07-22 23:03:08 +0530

    structure made for seat booking

---
+++ frontend/src/Components/BusBooking/BusBookingDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from './BusBookingDetails.module.css'
+
+function BusBookingDetails() {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export { BusBookingDetails }
