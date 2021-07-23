b81e1b69861c4c2c4c4872e9c7f3aa2d5815db03
info-vivekranjan <75258584+info-vivekranjan@users.noreply.github.com>
2021-07-23 18:07:51 +0530

    Adding booking reducer to store

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,13 +1,15 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
+import { busBookingReducer } from "./Booking/reducer";
 import { busreducer } from "./Bus/reducer";
-import {fromreducer} from './From/reducer';
-import {toreducer} from './To/reducer';
+import { fromreducer } from './From/reducer';
+import { toreducer } from './To/reducer';
 
 const rootReducer = combineReducers({
-    Bus:busreducer,
-    From:fromreducer,
-    To:toreducer
+  Bus: busreducer,
+  From: fromreducer,
+  To: toreducer,
+  Booking: busBookingReducer,
 });
 
 const composeEnhancers =
