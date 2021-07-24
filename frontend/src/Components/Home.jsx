17957d27209350a39505564f60d40690b15371bc
Shalini Sharma <shalini07061997@gmsail.com>
2021-07-24 15:48:38 +0530

    login

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -3,23 +3,21 @@ import { BelowTravelBlogSection } from "./Content/BelowTravelBlogSection";
 import { BusesFromSection } from "./Content/BusesFromSection";
 import { TravelVlogSection } from "./Content/TravelVlogSection";
 import { NavList } from "../../src/Components/NavList/NavList";
-import {Navbar} from '../Components/Navbar/Navbar';
+import { Navbar } from "../Components/Navbar/Navbar";
 import { AboveFooterSection } from "../Components/Footer/AboveFooterSection";
 import { Nav } from "./Navbar/Nav";
-
 
 function Home() {
   return (
     <>
-    <Nav/>
-     <Navbar/>
+      <Nav />
+      <Navbar />
       <NavList />
       <BusesFromSection />
       <TravelVlogSection />
       <AboveFooterSection />
       <BelowTravelBlogSection />
-      
-      </>
+    </>
   );
 }
 
