6b8d6dd39acda5214363d54342cced2d57efa684
shubham007kumar <you@example.com>
2021-07-24 20:35:55 +0530

    backend

--- backend/src/Controller/bus.controller.js
+++ backend/src/Controller/bus.controller.js
@@ -13,18 +13,15 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
-  try {
-    const bus = await Bus.find()
-      .populate("operatorId", { name_operator: 1, _id: 0 })
-      .populate({ path: "fromId", select: "from" })
-      .lean()
-      .exec();
-
-    return res.status(200).json({ data: bus });
-  } catch (e) {
-    return res.status(400).send(e.message);
-  }
-});
+router.get('/',async (req,res) => {
+  const dta = req.body.bus
+  console.log(dta)
+  try{
+      const bus = await Bus.find().populate('operatorId',{name_operator:1,_id:0}).populate({path:'fromId',populate:{path:'boardpoint'}}).populate({path:'toId',populate:{path:'droppoint'}}).exec()
+      return res.status(200).json({data:bus})
+  }catch(e){
+      return res.status(400).send(e.message)
+  }    
+})
 
 module.exports = router;
