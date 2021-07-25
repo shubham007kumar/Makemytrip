c2e0b7f91b84c1c902c51efc4c95a2e66e372c98
Shalini Sharma <shalini07061997@gmsail.com>
2021-07-25 09:26:53 +0530

    for pulling

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -6,10 +6,20 @@ import { NavList } from "../../src/Components/NavList/NavList";
 import { Navbar } from "../Components/Navbar/Navbar";
 import { AboveFooterSection } from "../Components/Footer/AboveFooterSection";
 import { Nav } from "./Navbar/Nav";
+import { FloatNav } from "./Navbar/FloatNav";
+import {Menu, Burger} from '../Components/Navbar/SideBar';
+
 
 function Home() {
+  const [open, setOpen] = React.useState(false);
+  const node = React.useRef();
   return (
-    <>
+    <div>
+      <div ref={node}>
+        <Burger open={open } setOpen={setOpen} />
+        <Menu open={open} setOpen={setOpen} />
+      </div>
+      <FloatNav/> 
       <Nav />
       <Navbar />
       <NavList />
@@ -17,7 +27,7 @@ function Home() {
       <TravelVlogSection />
       <AboveFooterSection />
       <BelowTravelBlogSection />
-    </>
+    </div>
   );
 }
 
